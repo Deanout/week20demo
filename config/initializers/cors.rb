a2103e6a3e4842a8264a1319160009480a9a5ffa
Dean DeHart <DeHartDean@gmail.com>
2021-04-18 01:49:40 -0400

    Try app://. for Electron cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
-
+   #origins 'http://localhost:8080'
+    origins 'app://.'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
