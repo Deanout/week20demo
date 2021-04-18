5bcba39d2ecbe02d6c3e79ab9a7f6a420ffe46b3
Dean DeHart <DeHartDean@gmail.com>
2021-04-18 02:20:14 -0400

    Push localhost:8080 cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-   #origins 'http://localhost:8080'
-    origins 'app://.'
+    origins 'http://localhost:8080'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
